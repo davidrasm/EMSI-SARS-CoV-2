22235fcc6befff0b95e89dd181460315df032b7b
drasmus@ncsu.edu <davidrasm@gmail.com>
2021-05-24 09:14:11 -0400

    Initial code and data commit

---
+++ code/plot_site_del_freqs.py
@@ -0,0 +1,97 @@
+"""
+Created on Fri Feb 12 06:37:59 2021
+
+Runs bioinformatic pipeline for global trees/seqs from GISAID
+
+@author: david
+"""
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from Bio import AlignIO
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+
+def make_features_map():
+    
+    features = ['nsp01','nsp02','nsp03','nsp04'
+		,'nsp05','nsp06','nsp07','nsp08'
+		,'nsp09','nsp10','nsp11','nsp12'
+		,'nsp13','nsp14','nsp15','nsp16'
+		,'Spike','ORF03a','ORF04','ORF05','ORF06'
+		,'ORF07a','ORF07b','ORF08','ORF09','ORF10']
+    start_locs = [266,806,2720,8555,10055,10973,11843,12092,12686,13025,13442,13442,16237,18040,19621
+    			,20659,21563,25393,26245,26523,27202,27394,27756,27894,28274,29558]
+    end_locs = [805,2719,8554,10054,10972,11842,12092,12685,13024,13441,13483,16236,18039,19620,20658
+    			,21555,25384,26220,26472,27192,27387,27759,27887,28259,29533,29674]
+    feature_data = {'Feature':features,'StartPosition': start_locs, 'EndPosition': end_locs}   
+    feature_df = pd.DataFrame(feature_data)
+    feature_df.to_csv('hcov_genomic_features.csv',index=False)
+
+def add_feature(ax, label, start, end, offset_cntr):
+    
+    "Add gene or feature annotation to plot ax from start to end"
+    left = start
+    bottom = (-0.2 - (offset_cntr*(0.1))) * ax.get_ylim()[1]
+    width = end - start
+    height = 0.08 * ax.get_ylim()[1]
+    p = patches.Rectangle(
+        (left, bottom), width, height,
+        fill=True, color='gray', transform=ax.transData, clip_on=False, alpha=0.75
+        )
+    ax.add_patch(p)
+    ax.text(left+(width/2), bottom+(height/2), label,
+        horizontalalignment='center',
+        verticalalignment='center',
+        transform=ax.transData)
+
+"""
+    Set up directories and files
+"""
+
+"Set directories"
+base_dir = Path(__file__).parent / "covid-analysis"
+tree_dir = base_dir / "phylogenies" / 'GISAID-hCoV-19-phylogeny-2021-03-08'
+align_dir = Path.home() / 'Desktop' / 'msa_0314'
+
+"Subsampled data set file names"
+#aln_fasta_file = str(align_dir / "hcov_USA_post2020-09-01_EMSI_subsampled_aligned.fasta")
+aln_fasta_file = str(align_dir / "hcov_march2021_USA_post2020-09-01_aligned.fasta")
+
+
+feature_df = pd.read_csv('hcov_genomic_features.csv',sep=",",index_col='Feature')
+
+"Count deletions at each site"
+align = AlignIO.read(aln_fasta_file, "fasta")
+n_sites = align.get_alignment_length()
+n_samples = len(align[:,0])
+deletions = []
+for i in range(n_sites):
+    print(i)
+    deletions.append(align[:,i].count('-'))
+site_del_freq = np.array(deletions) / n_samples 
+site_del_freq[:200] = np.nan # ignore first 200 sites
+site_del_freq[-200:] = np.nan # ignore last 200 sites
+
+"Plot deletion freqs across genome"
+sns.set(style="darkgrid")
+fig, ax = plt.subplots(1, 1)
+ax.plot(site_del_freq)
+#ax.set_xlabel('Position')
+ax.set_ylabel('Deletion Frequency')
+ax.grid(True)
+cntr = 0
+for feature, row in feature_df.iterrows():
+    add_feature(ax,feature, row['StartPosition'], row['EndPosition'],cntr) # add genomic feature annotation
+    cntr += 1
+    if cntr > 3: cntr = 0
+fig.set_size_inches(10, 6)
+fig.tight_layout()
+plt.show()
+
+fig.savefig('hcov_USA_post2020-09-01_site_del_freqs.png', dpi=200)
+
+
+
+ 
---
+++ code/subsample_GISAID_seqs.py
@@ -0,0 +1,239 @@
+"""
+Created on Fri Feb 12 06:37:59 2021
+
+Runs bioinformatic pipeline for global trees/seqs from GISAID
+
+@author: david
+"""
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import dendropy
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio import AlignIO
+from Bio.SeqRecord import SeqRecord
+import time
+from DateTimeUtils import date2FloatYear
+import subprocess
+import sys
+import re
+import Alignment
+
+
+def get_geographic_locs(df,column_name,mapToRegion=False):
+    
+    path = './covid-analysis/maps/'
+    code_map_file = path + 'code2StateMap.csv'
+    state_map_file = path + 'state2HHSRegionMap.csv'
+    
+    code_df = pd.read_csv(code_map_file, index_col=0)
+    codeMap = {index:row['State'] for index, row in code_df.iterrows()}
+    
+    state_df = pd.read_csv(state_map_file, index_col=0)
+    stateMap = {index:row['Region'] for index, row in state_df.iterrows()}
+    
+    drop_indexes = []
+    locations = []
+    for index, row in df.iterrows():
+        
+        gloc = row['virus_name'].split('/')[2] # after 2nd backslash
+        gloc = gloc[:2] # first two chars should be state
+        
+        if gloc in codeMap:
+            state_loc = codeMap[gloc]
+            if mapToRegion:
+                mapped_loc = stateMap[state_loc]
+            else:
+                mapped_loc = state_loc
+        else:
+            mapped_loc = 0 # 0 represents unknown
+            print("Unrecognized geographic location: " + gloc)
+        
+        locations.append(mapped_loc)
+    
+    df[column_name] = locations
+    df.drop(drop_indexes,inplace=True) # drop unknowns
+        
+    return df
+
+def fasta2df(fasta_file):
+    
+    "Get sequences from fasta"
+    seq_dic = {}
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        seq_dic[record.id] = [i for i in record.seq]
+    df = pd.DataFrame.from_dict(seq_dic, orient='index')
+    
+    return df
+
+def date_tree(tree_file,tip_date_file,rate_file,verbose = False):
+    
+    cluster = False
+    if cluster:
+        cmd = '~/lsd -i ' + tree_file + ' -d ' + tip_date_file + ' -c -w ' + rate_file
+    else:
+        cmd = 'lsd -i ' + tree_file + ' -d ' + tip_date_file + ' -c -w ' + rate_file
+    try:
+        output = subprocess.check_output(cmd, shell=True,stderr=subprocess.STDOUT)
+        if verbose:
+            sys.stdout.write(output)
+    except subprocess.CalledProcessError as exc:
+        print(exc.output)
+        print('Execution of "%s" failed!\n' % cmd)
+        sys.exit(1)
+        
+    "Parse newick tree and remove date annotation"
+    f = open(tree_file + '.result.date.nexus')
+    line = f.readline()
+    while line:
+        if "tree 1 =" in line:
+            tree = line.split()[3]
+        line = f.readline()
+    f.close()
+    tree = re.sub("[\[].*?[\]]", "", tree) # remove bracketed dates
+    out_tree = tree_file.split('.')[0] + '_dated.tre'
+    nwk=open(out_tree,"w")
+    nwk.write(tree + '\n')
+    nwk.close()    
+
+def write_tip_dates(tip_date_dict,date_file):
+    
+    txt=open(date_file,"w")
+    txt.write(str(len(tip_date_dict)) + '\n')
+    for k,v in tip_date_dict.items():
+        txt.write(k + '\t' + str(v) + '\n')
+    txt.close()
+    
+def rename_seqs(df,records):
+    
+    "Iterate through list of records, renaming each as we go"
+    new_records = []
+    for rec in records:
+        date = df.loc[rec.name]['collection_date']
+        del_state = df.loc[rec.name]['nsp6_Delta9']
+        if del_state:
+            del_state = 'Present'
+        else:
+            del_state = 'Absent'
+        rec.id = rec.name + '_' + del_state + '_' + date
+        rec.description = '' # set description blank
+        new_records.append(rec)
+
+    return new_records
+
+def get_del_state(df,align_file,column_name):
+    
+    "This is specifically hardcoded the ORF6 del9 deletion"
+    seq_dict = SeqIO.to_dict(SeqIO.parse(align_file, "fasta"))
+    deletions = []
+    for index, row in df.iterrows():
+        rec = seq_dict.get(index)
+        seq_str = str(rec.seq[11287:11296]) # genomic start pos is shifted -1 for zero-based indexing
+        del_str = '-'*9
+        if seq_str == del_str:
+            deletions.append(1)
+        else:
+            deletions.append(0)
+            
+    df[column_name] = deletions
+    
+    return df
+
+def subsample_align(sampled_taxa,aln_file,new_aln_file):
+    
+    "Get sequences in tree from GISAID fasta file"
+    seq_dict = SeqIO.to_dict(SeqIO.parse(aln_file, "fasta")) # one line alternative
+    seq_records = []
+    missing_taxa = []
+    for tx in sampled_taxa:
+        rec = seq_dict.get(tx)
+        if rec is not None:
+            seq_records.append(rec)
+        else:
+            missing_taxa.append(tx)
+            print('WARNING: ' + tx + " is not in GISAID sequence file")
+    SeqIO.write(seq_records, new_aln_file, "fasta")
+
+"""
+    Set up directories and files
+"""
+
+"Set directories"
+base_dir = Path(__file__).parent / "covid-analysis"
+tree_dir = base_dir / "phylogenies" / 'GISAID-hCoV-19-phylogeny-2021-03-08'
+align_dir = Path.home() / 'Desktop' / 'msa_0314'
+
+"GISAID input files"
+#tree_file = str(tree_dir / "hcov_march2021_USA_post2020-09-01.tree")
+
+"Subsample full sequence data to get smaller EMSI data set"
+meta_file = str(tree_dir / 'hcov_USA_post2020-09-01_EMSI_metadata.csv')
+aln_fasta_file = str(align_dir / "hcov_march2021_USA_post2020-09-01_aligned.fasta")
+sub_meta_file = str(tree_dir / "hcov_USA_post2020-09-01_EMSI_subsampled_metadata.csv")
+sub_aln_file = str(align_dir / "hcov_USA_post2020-09-01_EMSI_subsampled_aligned.fasta")
+meta_df = pd.read_csv(meta_file,sep=",",index_col='accession_id')
+sample_count = 2000
+sub_df = meta_df.sample(n=sample_count, axis=0)
+sub_df.to_csv(sub_meta_file,index=False)
+sampled_taxa = sub_df.index.tolist()
+subsample_align(sampled_taxa,aln_fasta_file,sub_aln_file)
+
+# "Subsampled data set file names"
+# meta_file = str(tree_dir / "hcov_USA_post2020-09-01_EMSI_subsampled_metadata.csv")
+# aln_fasta_file = str(align_dir / "hcov_USA_post2020-09-01_EMSI_subsampled_aligned.fasta")
+
+# """
+#     Create new sub_sampled data set
+# """
+
+# "Get metadata for GISAID global tree"
+# meta_df = pd.read_csv(meta_file,sep=",",index_col='accession_id')
+
+# "Encode deletion as binary variable in meta data"
+# del_label = 'nsp6_Delta9'
+# meta_df = get_del_state(meta_df,aln_fasta_file,del_label)
+
+# "Split df in presence/absence of deletion"
+# sub_df = meta_df[meta_df['nsp6_Delta9'] == 0]
+# del_sub_df = meta_df[meta_df['nsp6_Delta9'] == 1]
+
+# "Subsample seqs without deletion"
+# sample_count = len(del_sub_df.index)
+# sub_df = sub_df.sample(n=sample_count, axis=0)
+
+# "Merge back into one dataframe"
+# sub_df = sub_df.append(del_sub_df)
+
+# "Get subsampled alignment for samples in sub_df"
+# sampled_taxa = sub_df.index.tolist()
+# new_aln_file = str(align_dir / "hcov_USA_post2020-09-01_EMSI_nsp6_Delta9_subsampled_aligned.fasta") 
+# subsample_align(sampled_taxa,aln_fasta_file,new_aln_file)
+
+# "Rename records in fasta file by accession id to match taxa labels in tree"
+# records = SeqIO.parse(new_aln_file, "fasta")
+# records = rename_seqs(sub_df,records) # reanme by accession id
+# SeqIO.write(records,new_aln_file, "fasta")
+
+"Extract tree of desired samples using dendropy"
+# filtered_taxa = set(filtered_taxa) # index is 'accession_id'
+# filtered_taxa = filtered_taxa.difference(set(missing_taxa)) # remove missing taxa
+# filtered_taxa_spaces = [t.replace('_',' ') for t in filtered_taxa] # need to replace underscores with spaces to match dendropy taxon labels
+# taxa = dendropy.TaxonNamespace()
+# global_tree = dendropy.Tree.get(file=open(tree_file, 'r'), schema="newick", rooting="default-rooted", taxon_namespace=taxa) 
+# if not global_tree.is_rooted:
+#     print('WARNING: Global tree is not rooted') # Should be rooted, this is just to check
+# taxa_to_retain = set([taxon for taxon in global_tree.taxon_namespace if taxon.label in filtered_taxa_spaces])
+# filtered_tree = global_tree.extract_tree_with_taxa(taxa=taxa_to_retain)
+# filtered_tree.write(path=filtered_tree_file,schema='newick',suppress_annotations=True,suppress_rooting=True)
+
+"Get tip dates file for dating with lsd and date tree"
+# tip_date_dict = {}
+# for sample, row in meta_df.iterrows():
+#  	tip_date_dict[sample] = date2FloatYear(row['collection_date'])
+# write_tip_dates(tip_date_dict,tip_date_file)
+# date_tree(filtered_tree_file,tip_date_file,rate_file)
+
+
+
+ 
